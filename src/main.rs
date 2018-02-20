7addc26700ebdfadab85236212243999f4176515
Cindy <czwang@bu.edu>
2018-02-20 01:09:26 -0800

    Error messages

--- src/main.rs
+++ src/main.rs
@@ -46,14 +46,14 @@ fn main() {
                     .get_matches();
     
     let domain: &str = "https://www.yelp.com";
-    let url_arg = matches.value_of("url").unwrap();
+    let url_arg = matches.value_of("url").expect("URL is required");
     // strip off the "&ns=1"
     let url = match url_arg.rfind("&ns=1") {
         Some(v) => url_arg[..v].to_owned() + "&rpp=40",
         None => url_arg.to_owned() + "&rpp=40",
     };
 
-    let start_index = url.find("/search?").unwrap();
+    let start_index = url.find("/search?").expect("Your URL did not have a search query");
     let init_query = &url[start_index..];
 
     let out_path = matches.value_of("output").unwrap_or("./out.txt");
@@ -65,7 +65,7 @@ fn main() {
     };
 
     let mut table = Table::new();
-    let mut out = File::create(out_path).unwrap();
+    let mut out = File::create(out_path).expect("Failed to create file");
 
     let mut yelp_business_links = vec![];
     let client = Client::new();
@@ -81,39 +81,42 @@ fn main() {
     bar.finish();
 
     table.printstd();
-    write!(out, "{:?}\n", keywords).unwrap();
-    table.to_csv(out).unwrap();
+    write!(out, "{:?}\n", keywords).expect("Failed to write to file");
+    table.to_csv(out).expect("Failed to write to file");
     println!("\nSearched Yelp reviews for keywords: {:?}", keywords);
     println!("Output file at {}", out_path);
     let time_elapsed = started.elapsed();
-    println!("Time elasped: {}:{}", time_elapsed.as_secs() / 60, time_elapsed.as_secs() % 60);
+    println!("Time elasped: {}:{:02}", time_elapsed.as_secs() / 60, time_elapsed.as_secs() % 60);
 }
 
 
 fn get_yelp_index_links(client: &Client, domain: &str, query: &str, yelp_links: &mut Vec<(String, String)>) {
     let url = domain.to_owned() + query;
-    let mut resp = client.get(&url).send().unwrap();
+    let mut resp = client.get(&url).send().expect("Connection error: index page");
     assert!(resp.status().is_success());
 
-    let body = resp.text().unwrap();
+    let body = resp.text().expect("Could not get document for index page");
     let fragment = Html::parse_document(&body);
-    let businesses = Selector::parse("li.regular-search-result a.biz-name").unwrap();
+    let businesses = Selector::parse("li.regular-search-result a.biz-name").expect("Not a valid css selector");
 
     for business in fragment.select(&businesses) {
         let business_name = business.text().collect::<Vec<_>>()[0].trim();
 
         // get the business's yelp page
-        let mut rel_path = business.value().attr("href").unwrap();
-        let end_index = rel_path.rfind('?').unwrap();
-        let business_yelp_link = domain.to_owned() + &rel_path[0..end_index];
-        yelp_links.push((business_name.to_owned(), business_yelp_link));
+        if let Some(rel_path) = business.value().attr("href") {
+            if let Some(end_index) = rel_path.rfind('?') {
+                let business_yelp_link = domain.to_owned() + &rel_path[0..end_index];
+                yelp_links.push((business_name.to_owned(), business_yelp_link));
+            }
+        }
     }
 
     // do it again on the next page
-    let next_page_selector = Selector::parse("div.search-pagination a.next.pagination-links_anchor").unwrap();
+    let next_page_selector = Selector::parse("div.search-pagination a.next.pagination-links_anchor").expect("Not a valid css selector");
     for next_page_link in fragment.select(&next_page_selector) {
-        let rel = next_page_link.value().attr("href").unwrap();
-        get_yelp_index_links(client, domain, &rel, yelp_links);
+        if let Some(rel) = next_page_link.value().attr("href") {
+            get_yelp_index_links(client, domain, &rel, yelp_links);
+        }
     }
 }
 
@@ -122,17 +125,18 @@ fn search_reviews(client: &Client, yelp_business_page_url: &str, keywords: &Vec<
     let mut count: u32 = 0;
     for keyword in keywords.iter() {
         let url = yelp_business_page_url.to_owned() + "?q=" + keyword;
-        let mut resp = client.get(&url).send().unwrap();
-        let body = resp.text().unwrap();
+        let mut resp = client.get(&url).send().expect("Connection error: business page");
+        let body = resp.text().expect("Could not get document for business page");
         let fragment = Html::parse_document(&body);
 
-        let num_reviews_selector = Selector::parse("div.feed div.feed_filters h3.feed_search-results").unwrap();
+        let num_reviews_selector = Selector::parse("div.feed div.feed_filters h3.feed_search-results").expect("Not a valid css selector");
         for header in fragment.select(&num_reviews_selector) {
             let num_text_v = header.text().collect::<Vec<_>>();
             let num_text = &num_text_v[0].trim();
-            let idx = num_text.find(' ').unwrap();
-            let num = &num_text[0..idx].parse().unwrap();
-            count += num;
+            if let Some(idx) = num_text.find(' ') {
+                let num = &num_text[0..idx].parse().unwrap_or(0);
+                count += num;
+            }
         }
     }
     count
